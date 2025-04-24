2b2f2a0811045467cc5c7c59a5bd915e104b6fc6
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-04-24 15:56:46 -0600

    todo nuevo

--- blog/bWeb/script.js
+++
@@ -1,8 +0,0 @@
-function moverCuadro() {
-    const cuadro = document.getElementById('cuadro');
-    const nuevaPosicionTop = Math.random() * (window.innerHeight - 100);
-    const nuevaPosicionLeft = Math.random() * (window.innerWidth - 100);
-
-    cuadro.style.top = nuevaPosicionTop + 'px';
-    cuadro.style.left = nuevaPosicionLeft + 'px';
-}
